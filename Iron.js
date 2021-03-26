de1ecb6b127cc45e60e70b72414580bc5d1f3850
Rakin123 <77465359+Rakin123@users.noreply.github.com>
2021-03-25 20:39:18 -0400

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,27 @@
+class Iron{
+    constructor(x,y){
+        var options = {
+            'restitution':0.8,
+            'friction':3,
+            'density':30
+        };
+        this.body = Bodies.rectangle(x,y,75,37.5,options);
+        this.width = 75;
+        this.height = 37.5;
+
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        strokeWeight(3);
+        stroke("white");
+        fill("red");
+        rectMode(CENTER);
+        rect(0,0,this.width,this.height);
+        pop();
+    };
+};
---
+++ sketch.js
@@ -0,0 +1,39 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+var stone;
+var moltenIron;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+
+    rubber = new Rubber(700,400,50)
+    stone  = new Stone(400,400,75,100);
+    moltenIron = new Iron(300, 500);
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+    
+
+    plane.display();
+    rubber.display();
+    stone.display();
+    hammer.display();
+    moltenIron.display();
+
+    
+ 
+}
